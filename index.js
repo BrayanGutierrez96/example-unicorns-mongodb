d807092dd34162a387d753d47486df4e8d2903cf
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-08-25 17:46:07 -0500

    intalacion de cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import { PORT } from "./src/config.js";
 import { conexion } from "./src/db.js";
 import { unicornioRoute } from "./src/routes/unicornio.route.js";
+import cors from 'cors'
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(unicornioRoute);
 
